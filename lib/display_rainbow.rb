a8f25657eab310744fc1f1b85407ec2ccf58cba0
Christopher Cabell <cccabell@icloud.com>
2019-05-10 18:20:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts "R: #{colors[0]}, O: orange, Y: #{colors[2]}, G: #{colors[2]}"
+  puts "R: #{colors[0]}, O: orange, Y: #{colors[2]}, G: #{colors[2]}, B: #{colors[2]}, I: #{colors[2]}, V: #{colors[2]}"
 end
